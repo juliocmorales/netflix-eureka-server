2a09ace4248bdb8571ad9e3c571db5d8fd57fa2c
juliocmorales <40004698+juliocmorales@users.noreply.github.com>
2018-07-06 13:01:55 -0500

    primer commit

---
+++ src/main/java/com/neoris/netflix/NetflixApplication.java
@@ -0,0 +1,14 @@
+package com.neoris.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApplication.class, args);
+	}
+}
